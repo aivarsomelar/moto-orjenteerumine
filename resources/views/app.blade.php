98cc333278b7742ab14fe55388f5ab37ae4340b6
Aivar Somelar <aivarsomelar@gmail.com>
2015-05-30 15:28:10 +0300

    Load my own style sheet into web page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
 	<title>URX</title>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('/css/urx.css') }}" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
