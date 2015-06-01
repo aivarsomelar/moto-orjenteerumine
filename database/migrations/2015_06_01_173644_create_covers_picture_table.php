8c8286922626be14f857d61ade097147e60a5d54
Aivar Somelar <aivarsomelar@gmail.com>
2015-06-01 20:40:05 +0300

    Create tables for profile pictures and covers

---
+++ database/migrations/2015_06_01_173644_create_covers_picture_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoversPictureTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('covers', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('file_name');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('covers');
+	}
+
+}
