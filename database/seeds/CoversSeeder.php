a846a3b76c64edd1ebf7bf0c7633aa4deef54879
Aivar Somelar <aivarsomelar@gmail.com>
2015-06-03 21:31:36 +0300

    Create covers table seeder

---
+++ database/seeds/CoversSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Seed cover table with picture names
+ */
+class CoversSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Model::unguard();
+
+		DB::table('covers')->delete();
+		DB::table('covers')->insert(
+            [
+                ['file_name' => 'Audi_R8_city.jpg'],
+                ['file_name' => 'Black_BMW_E46.jpg'],
+                ['file_name' => 'bmw-m5-car.jpg'],
+                ['file_name' => 'car-rims.jpg'],
+                ['file_name' => 'dodge2.jpg'],
+                ['file_name' => 'Dodge-Viper-SRT_desert.jpg'],
+                ['file_name' => 'drift1.jpg'],
+                ['file_name' => 'lamborghini_aventador2.jpg'],
+                ['file_name' => 'mercedes-slr.jpg'],
+                ['file_name' => 'nissanZ.jpg'],
+                ['file_name' => 'scirocco-front.jpg'],
+                ['file_name' => 'Subaru_en2.jpg'],
+                ['file_name' => 'subaru_impreza2.jpg'],
+                ['file_name' => 'subaru_impreza_wrx1.jpg'],
+                ['file_name' => 'wall24.jpg']
+            ]
+        );
+	}
+}
