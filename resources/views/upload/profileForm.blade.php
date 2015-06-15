1ee991a1e88f41e059c24f7bc044ef44a105e888
Aivar Somelar <aivarsomelar@gmail.com>
2015-06-15 19:59:23 +0300

    Add upload form for profile picture upload

--- resources/views/upload/profileForm.blade.php
+++ resources/views/upload/profileForm.blade.php
@@ -4,7 +4,7 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Lae ülesse "cover" pilt</div>
+				<div class="panel-heading">Lae ülesse profiili pilt</div>
 
 				<div class="panel-body">
                     @if($errors->first())
