b5cc2337f3d6a88e0e86685a8ba545c0ec1e2a5a
Aivar Somelar <aivarsomelar@gmail.com>
2015-06-16 21:09:51 +0300

    Add error/message shower

--- resources/views/pictures/allProfile.blade.php
+++ resources/views/pictures/allProfile.blade.php
@@ -3,6 +3,16 @@
 @section('content')
 
 <div class="row pic-margin">
+    @if($errors->first())
+        <div class="alert alert-danger" role="alert">
+            {{$errors->first()}}
+        </div>
+    @endif
+    @if(\Illuminate\Support\Facades\Session::get('message'))
+        <div class="alert alert-success" role="alert">{{\Illuminate\Support\Facades\Session::get('message')}}</div>
+        {{\Illuminate\Support\Facades\Session::forget('message')}}
+    @endif
+
     <div class="col-sm-offset-10">
         <a href="{{url('/picture/profile/form')}}">
             <button class="btn btn-margin btn-success">Lae ülesse uus profiili pilt</button>
